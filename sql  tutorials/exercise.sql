343d37db3d617dfaf4ccdfb104ba998afc3800e6
devAbhijit777 <73705314+devAbhijit777@users.noreply.github.com>
2025-04-24 03:50:12 +0000

    exercise

--- sql  tutorials/exercise.sql
+++ sql  tutorials/exercise.sql
@@ -1,1 +1,12 @@
-select * from employees
+--
+--1.  selcting rows ,
+--2.  subset of rows, 
+--3.  columns ,
+--4.  subset of columns
+--5.  Giving meaningful names to columns
+--6.  Referencing an Aliased Column in the WHERE Clause
+--7. Concatenating column values
+--select * from employees
+select emp_name + ' has salary of ' + salary as details from employees
+-- concat(emp_name , ' has salary of ' , salary) as details from employees
+
